06c801ee3be8e5d9d21c79d454e14c027bc7a380
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-29 15:33:50 +0200

    Dashboard and PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,14 @@ import store from './redux/store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './redux/auth/actions';
 
-import Register from './components/auth/Register';
 import { Landing } from './components/layout/Landing';
+import Register from './components/auth/Register';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 //
 if (localStorage.token) {
@@ -37,6 +40,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
